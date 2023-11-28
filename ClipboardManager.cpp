3bc9a00c362f6c6160676c2a1c572144280c9561
Yaopeng Xie <yxie447@uwo.ca>
2023-11-28 11:57:41 -0500

    version 1.05 - add comments

--- ClipboardManager.cpp
+++ ClipboardManager.cpp
@@ -1,13 +1,34 @@
+/**
+ * @file ClipboardManager.cpp
+ * @brief Implementation file for the ClipboardManager class.
+ *
+ * Implements the methods defined in ClipboardManager.h. This file contains the functional logic for managing
+ * clipboard operations in a Windows environment, such as pushing text to the clipboard, retrieving clipboard contents,
+ * and checking for clipboard changes.
+ *
+ * @author Yaopeng Xie
+ * @date 2023/11/14
+ */
+
 #include "ClipboardManager.h"
 #include <iostream>
 #include <iomanip>
 
-// Implement the ClipboardContent class methods
+/**
+ * @brief Constructs a ClipboardContent object.
+ *
+ * Initializes a new instance of the ClipboardContent class with specified data type, text data, binary data, and update time.
+ *
+ * @param type The type of the clipboard data (Text, Image, File, etc.).
+ * @param textData The text content of the clipboard data.
+ * @param binaryData The binary content of the clipboard data.
+ * @param updateTime The timestamp when the clipboard data was last updated.
+ */
 ClipboardContent::ClipboardContent(ClipboardDataType type, const std::wstring& textData,
                                    const std::vector<unsigned char>& binaryData, const std::string& updateTime)
         : dataType(type), textContent(textData), binaryContent(binaryData), updateTime(updateTime) {}
 
-
+// Implementations for getter and setter functions
 ClipboardDataType ClipboardContent::getDataType() const {
     return dataType;
 }
@@ -40,12 +61,23 @@ void ClipboardContent::setUpdateTime(const std::string& updateTime) {
     this->updateTime = updateTime;
 }
 
+
+
 // Initialize the static member of ClipboardManager
 DWORD ClipboardManager::lastClipboardSequenceNumber = GetClipboardSequenceNumber();
 
 std::string ClipboardManager::lastUpdateTime = "";
 
-// Implement the ClipboardManager class methods
+// ClipboardManager class methods
+
+/**
+ * @brief Checks if the clipboard content has changed.
+ *
+ * Compares the current clipboard sequence number against the last known sequence number to determine if the clipboard content has changed.
+ * Outputs the current and last sequence numbers for debugging purposes.
+ *
+ * @return True if the clipboard content has changed, false otherwise.
+ */
 bool ClipboardManager::HasClipboardChanged() {
     DWORD currentSequenceNumber = GetClipboardSequenceNumber();
     std::cout << "Current Seq Num: " << currentSequenceNumber << ", Last Seq Num: " << lastClipboardSequenceNumber << std::endl; // Debug print
@@ -58,6 +90,13 @@ bool ClipboardManager::HasClipboardChanged() {
     return false;
 }
 
+/**
+ * @brief Gets the current time as a string.
+ *
+ * Retrieves the current system time and formats it as a human-readable string.
+ *
+ * @return A string representing the current system time.
+ */
 std::string ClipboardManager::GetCurrentTimeAsString() {
     auto now = std::chrono::system_clock::now();
     auto now_c = std::chrono::system_clock::to_time_t(now);
@@ -66,6 +105,14 @@ std::string ClipboardManager::GetCurrentTimeAsString() {
     return stream.str();
 }
 
+/**
+ * @brief Gets the window handle to the current clipboard owner.
+ *
+ * Opens the clipboard and retrieves the handle to the window that currently owns the clipboard.
+ * If the clipboard is successfully opened, it is then closed after retrieving the handle.
+ *
+ * @return The handle to the clipboard owner window if the clipboard is successfully opened, otherwise NULL.
+ */
 HWND ClipboardManager::GetClipboardWindowHandle() {
     if (OpenClipboard(NULL)) {
         HWND hWindow = GetClipboardOwner();
@@ -75,10 +122,26 @@ HWND ClipboardManager::GetClipboardWindowHandle() {
     return NULL;
 }
 
+/**
+ * @brief Gets the last update time of the clipboard content.
+ *
+ * Retrieves the timestamp of when the clipboard content was last updated.
+ * This is a static method providing access to the static member lastUpdateTime.
+ *
+ * @return A string representing the last update time of the clipboard content.
+ */
 std::string ClipboardManager::GetLastUpdateTime() {
     return lastUpdateTime;
 }
 
+/**
+ * @brief Gets the format of the current data on the clipboard.
+ *
+ * This method checks the clipboard and identifies the format of the data currently stored on it.
+ * Supported formats include CF_UNICODETEXT for text, CF_DIB for images, and CF_HDROP for file handles.
+ *
+ * @return The clipboard format identifier of the current clipboard data, or 0 if no known format is available.
+ */
 UINT ClipboardManager::GetClipboardFormat() {
     if (IsClipboardFormatAvailable(CF_UNICODETEXT)) {
         return CF_UNICODETEXT;
@@ -91,6 +154,16 @@ UINT ClipboardManager::GetClipboardFormat() {
     }
 }
 
+/**
+ * @brief Sets the given text onto the clipboard.
+ *
+ * This method places a Unicode text string onto the system clipboard.
+ * It opens the clipboard, allocates global memory for the text, copies the text, and sets it on the clipboard.
+ * If successful, the text can be pasted from the clipboard into other applications.
+ *
+ * @param hWindow A handle to the window that currently owns the clipboard.
+ * @param s The Unicode text string to be placed onto the clipboard.
+ */
 void ClipboardManager::SetClipboardText(HWND hWindow, const std::wstring &s) {
     if (OpenClipboard(hWindow)) {
         EmptyClipboard();
@@ -105,6 +178,15 @@ void ClipboardManager::SetClipboardText(HWND hWindow, const std::wstring &s) {
     }
 }
 
+/**
+ * @brief Retrieves text from the clipboard.
+ *
+ * This method fetches a Unicode text string from the system clipboard, if available.
+ * It opens the clipboard, gets the text data, and then closes the clipboard.
+ *
+ * @param hWindow A handle to the window that currently owns the clipboard.
+ * @return A Unicode string containing the text retrieved from the clipboard, or an empty string if no text is available.
+ */
 std::wstring ClipboardManager::GetClipboardText(HWND hWindow) {
     if (OpenClipboard(hWindow)) {
         HANDLE hData = GetClipboardData(CF_UNICODETEXT);
@@ -122,6 +204,15 @@ std::wstring ClipboardManager::GetClipboardText(HWND hWindow) {
     return L"";
 }
 
+/**
+ * @brief Retrieves the content of the clipboard based on its format.
+ *
+ * This method checks the current format of the clipboard and retrieves its content accordingly.
+ * The content can be either text, image, or file based on the identified format.
+ *
+ * @param hWindow A handle to the window that currently owns the clipboard.
+ * @return A ClipboardContent object containing the retrieved clipboard data.
+ */
 ClipboardContent ClipboardManager::GetClipboardContent(HWND hWindow) {
     UINT format = GetClipboardFormat();
 
@@ -144,6 +235,14 @@ ClipboardContent ClipboardManager::GetClipboardContent(HWND hWindow) {
     }
 }
 
+/**
+ * @brief Pushes a string to the system clipboard.
+ *
+ * This method handles the process of placing a plain text string onto the clipboard.
+ * It opens the clipboard, allocates a global memory block for the string, and sets the clipboard data.
+ *
+ * @param str The ASCII string to be pushed onto the clipboard.
+ */
 void ClipboardManager::PushStringToClipboard(const std::string& str) {
     if (OpenClipboard(NULL)) {
         EmptyClipboard(); // Clear the clipboard
--- ClipboardManager.h
+++ ClipboardManager.h
@@ -1,6 +1,11 @@
-//
-// Created by yxie447 on 2023/11/27.
-//
+/**
+ * @file ClipboardManager.h
+ * @brief Header file for the ClipboardManager class.
+ *
+ *
+ * @author Yaopeng Xie
+ * @date 2023/11/14
+ */
 
 #ifndef CLIPBOARDMANAGER_H
 #define CLIPBOARDMANAGER_H
@@ -11,20 +16,36 @@
 #include <chrono>
 #include <sstream>
 
-// Declare the enum class ClipboardDataType
+/**
+ * @brief Clipboard data types enumeration.
+ *
+ * Defines the types of data that can be stored in the clipboard.
+ */
 enum class ClipboardDataType {
-    Text,
-    Image,
-    File,
-    Unknown
+    Text,   ///< Represents text data
+    Image,  ///< Represents image data
+    File,   ///< Represents file data
+    Unknown ///< Represents an unknown or unsupported data type
 };
 
-// Declaration of ClipboardContent class
+/**
+ * @brief Encapsulates clipboard data and its type.
+ *
+ * This class is used to store clipboard data along with its type. It also stores the time when the data was updated.
+ */
 class ClipboardContent {
 public:
+    /**
+     * @brief Constructs a ClipboardContent object with given parameters.
+     * @param type The type of the clipboard data.
+     * @param textData The text data, if the type is Text.
+     * @param binaryData The binary data (for images or files).
+     * @param updateTime The time when the clipboard data was updated.
+     */
     ClipboardContent(ClipboardDataType type, const std::wstring& textData,
                      const std::vector<unsigned char>& binaryData, const std::string& updateTime = "");
 
+    // Getters and setters
     ClipboardDataType getDataType() const;
     void setDataType(ClipboardDataType type);
     std::wstring getTextData() const;
@@ -35,29 +56,111 @@ public:
     void setUpdateTime(const std::string& updateTime);
 
 private:
-    ClipboardDataType dataType;
-    std::wstring textContent; // For text data
-    std::vector<unsigned char> binaryContent; // For binary data (images, files)
-    std::string updateTime;
+    ClipboardDataType dataType; ///< Data type of the clipboard content
+    std::wstring textContent; ///< Text data content
+    std::vector<unsigned char> binaryContent; ///< Binary data content (images, files)
+    std::string updateTime; ///< Time when the clipboard content was updated
 };
 
-// Declaration of ClipboardManager class
+/**
+ * @brief Manages clipboard operations.
+ *
+ * This class provides static methods to interact with the system clipboard, including reading and writing content.
+ */
 class ClipboardManager {
 public:
+    /**
+    * @brief Pushes a string to the system clipboard.
+    * @param str The string to be pushed to the clipboard.
+    */
     static void PushStringToClipboard(const std::string& str);
+
+    /**
+     * @brief Checks if the clipboard content has changed.
+     * @return True if the clipboard content has changed, false otherwise.
+     */
     static bool HasClipboardChanged();
+
+    /**
+     * @brief Gets the current time as a string.
+     * @return The current time in a human-readable string format.
+     */
     static std::string GetCurrentTimeAsString();
+
+    /**
+     * @brief Retrieves the handle to the window that currently owns the clipboard.
+     *
+     * This method is used to obtain the handle of the window that is currently interacting with the clipboard.
+     * It is typically used in conjunction with other clipboard operations.
+     *
+     * @return The handle to the window owning the clipboard, or NULL if no window owns the clipboard.
+     */
     static HWND GetClipboardWindowHandle();
+
+    /**
+     * @brief Gets the format of the current data on the clipboard.
+     *
+     * This method checks the clipboard and identifies the format of the data currently stored on it.
+     * Supported formats include text, images, and file handles.
+     *
+     * @return The clipboard format identifier of the current clipboard data.
+     */
     static UINT GetClipboardFormat();
+
+    /**
+     * @brief Sets the given text onto the clipboard.
+     *
+     * This method places a Unicode text string onto the system clipboard. If successful,
+     * the text can be pasted from the clipboard into other applications.
+     *
+     * @param hWindow A handle to the window that currently owns the clipboard.
+     * @param s The Unicode text string to be placed onto the clipboard.
+     */
     static void SetClipboardText(HWND hWindow, const std::wstring &s);
+
+    /**
+     * @brief Retrieves text from the clipboard.
+     *
+     * This method fetches a Unicode text string from the system clipboard, if available.
+     *
+     * @param hWindow A handle to the window that currently owns the clipboard.
+     * @return A Unicode string containing the text retrieved from the clipboard.
+     */
     static std::wstring GetClipboardText(HWND hWindow);
+
+    /**
+     * @brief Retrieves the current content of the clipboard.
+     *
+     * This method fetches the current content of the clipboard and encapsulates it in a ClipboardContent object.
+     * The content can include text, images, or files.
+     *
+     * @param hWindow A handle to the window that currently owns the clipboard.
+     * @return A ClipboardContent object containing the current clipboard content.
+     */
     static ClipboardContent GetClipboardContent(HWND hWindow);
+
+    /**
+     * @brief Gets the last update time of the clipboard content.
+     *
+     * This method retrieves the timestamp of when the clipboard content was last updated.
+     *
+     * @return A string representing the last update time of the clipboard content.
+     */
     static std::string GetLastUpdateTime();
-    static std::string lastUpdateTime;
+
+    static std::string lastUpdateTime; ///< Last update time of the clipboard content
 
 private:
-    static DWORD lastClipboardSequenceNumber;
+    static DWORD lastClipboardSequenceNumber; ///< Last known clipboard sequence number
 
+    /**
+     * @brief Sets the data type of the clipboard content.
+     *
+     * This private method is used to set the type of data currently stored in the clipboard.
+     * It is utilized internally to manage and categorize clipboard data based on its type, such as text, image, or file.
+     *
+     * @param type The type of the clipboard data, as defined in the ClipboardDataType enum.
+     */
     void setDataType(ClipboardDataType type);
 };
 
